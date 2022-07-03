03afe73aed7dd92f96615f1fa5a1f7cfd3582f3e
Ivan Martinez <ivmartinezc@gmail.com>
2022-07-03 18:41:32 +0200

    responsive mobile: burger menu y primera versión incompleta

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -1,11 +1,18 @@
 import "./navbar.css";
-import StyledLink from "./StyledLink"
+import StyledLink from "./StyledLink";
 import { useState } from "react";
 import Dropdown from "./Dropdown";
 
+//TODO: mobile, para el navbar falta:
+// - arreglar posicion, bg-color y color del dropdown 
+// - reducir tamaño footer o de iconos de footer
+// - hero: problema del espacio a la derecha (ya lo resolví en su día en versión desktop)
+// - título y subtítulos más pequeños y centrados al screen mobile al tiempo que la foto muestra a Gema
+// - resto de las páginas!!
 
 
-//TODO: Home, falta: 
+
+//TODO: Home, falta:
 //      -arreglar missmatch en el height del navbar y el body respecto del background-image (si es que este es el problema): resuelto con pos absolute en vez de relative en .navbar
 //      -dar estilos a los a:hover en la barra de navegación y dropdown
 //      -animación para el título de la página y ponerle un text-shadow
@@ -13,7 +20,7 @@ import Dropdown from "./Dropdown";
 //      -arreglar el copyright 2022
 //      -ajustar el width del background-image al tamaño del viewport
 //TODO: arreglar StyledLink (no quita el text-decoration a las pseudo clases: visited, etc.)
-//TODO: componente Entry (creado en /common) que recibe datos del entry como props 
+//TODO: componente Entry (creado en /common) que recibe datos del entry como props
 //TODO: enrutar bien el NotFound (documentación de react-router v6)
 //TODO: navbar size should be the same across pages
 
@@ -30,13 +37,20 @@ export default function Navbar() {
       ],
     },
     { title: "Docencia y supervisión", slug: "/docencia-y-supervision" },
-    { title: "Asesorias y Colaboraciones", slug: "/asesorias-y-colaboraciones" },
-    ];
+    {
+      title: "Asesorias y Colaboraciones",
+      slug: "/asesorias-y-colaboraciones",
+    },
+  ];
 
   return (
     <nav className="navbar">
       <StyledLink className="logo" to="/"></StyledLink>
-      <ul>
+
+      <input id="burger-menu" type="checkbox" />
+      <label for="burger-menu" className="burger-label"></label>
+
+      <ul className="navbar-list">
         {items.map((item) =>
           item.submenu ? (
             <li
@@ -58,7 +72,9 @@ export default function Navbar() {
             </li>
           ) : (
             <li key={item.title}>
-              <StyledLink className="animated-menu-item" to={item.slug}>{item.title}</StyledLink>
+              <StyledLink className="animated-menu-item" to={item.slug}>
+                {item.title}
+              </StyledLink>
             </li>
           )
         )}
--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./globals.css";
-import "./section.css"
+import "./section.css";
 
-// scp -i /home/ivan/Desktop/bootcampweb-kc.pem -r build  ubuntu@54.164.117.110:/var/www/html 
+// scp -i /home/ivan/Desktop/bootcampweb-kc.pem -r build  ubuntu@54.164.117.110:/home/ubuntu
 
-//TODO: popover con la descripción de los proyectos
-
+//TODO: títulos de las secciones en azul
+//TODO: construye un popover activable con click (con librería floating-ui) contentivo de una imagen ilustrativa y la descripción del proyectos
+//revisión exhaustiva de los items con Gema
 //fotos en algunas secciones (i.e. Docencia, Proyectos) para darle + vida
 //Gema: revisión exhaustiva de los items, agregar nuevos items, corregir typos y demás
 //TODO: dar más contraste a estilos en general y aplicar ui principles
 //TODO: leer todo el código: identificar potenciales mejoras en naming, estructura de ficheros, legibilidad y no-redundancia o inutilización de propiedades y clases CSS
+
 
 
 ReactDOM.render(
